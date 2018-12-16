2a0cbc15e03ed89d503d57fec57ec1a1f9f0ad92
Peter <pkabai@csumb.edu>
2018-12-15 23:22:57 -0800

    Adds more to template html

--- flaskApps/testApp/app.py
+++ flaskApps/testApp/app.py
@@ -1,14 +1,16 @@
+# run with cmmand: FLASK_APP=app.py flask run
+
 from flask import Flask, request, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def on_load():
-    html = render_template('template.html')
+    html = render_template('template.html', name='')
     return html
 
 @app.route('/', methods=['POST'])
 def after_post():
     text = request.form['text']
-    html = render_template('template.html')
-    return html + "<p style='color:blue;'>" + text + "</p>"
+    html = render_template('template.html', name=text)
+    return html
